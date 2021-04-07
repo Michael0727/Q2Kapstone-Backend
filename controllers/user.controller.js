183d07308a1c3c811f13a888649418ef905ec61c
Alex Mourtos <amourtos@gmail.com>
2021-04-07 10:27:51 -0400

    user sign up/user login complete

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,27 +1,16 @@
 const User = require("../models/user.model");
 const router = require("express").Router();
 const express = require("express");
-const { check, validationResult } = require("express-validator/check");
+const { check, validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-
-// router.route("/new").post((req, res) => {
-//   const newUser = new User(req.body);
-
-//   newUser
-//     .save()
-//     .then((user) => res.json(user))
-//     .catch((err) => res.status(400).json("Error!" + err));
-// });
 
 router.post(
   "/signup",
   [
-    check("username", "Please Enter a Valid Username").not().isEmpty(),
-    check("email", "Please enter a valid email").isEmail(),
-    check("password", "Please enter a valid password").isLength({
-      min: 6,
-    }),
+    check("username", "please enter a valid username").not().isEmpty(),
+    check("email", "Please Enter a Valid Email").isEmail(),
+    check("password", "Please enter a valid password").isLength({ min: 6 }),
   ],
   async (req, res) => {
     const errors = validationResult(req);
@@ -30,48 +19,29 @@ router.post(
         errors: errors.array(),
       });
     }
-
     const { username, email, password } = req.body;
     try {
       let user = await User.findOne({
         email,
       });
       if (user) {
-        return res.status(400).json({
-          msg: "User Already Exists",
-        });
+        return res.status(400).json({ msg: "User Already Exists" });
       }
-
       user = new User({
         username,
         email,
         password,
       });
-
-      const salt = await bcrypt.genSalt(10);
+      const salt = await bcrypt.genSalt();
       user.password = await bcrypt.hash(password, salt);
-
       await user.save();
-
       const payload = {
-        user: {
-          id: user.id,
-        },
+        user: { id: user.id },
       };
-
-      jwt.sign(
-        payload,
-        "randomString",
-        {
-          expiresIn: 10000,
-        },
-        (err, token) => {
-          if (err) throw err;
-          res.status(200).json({
-            token,
-          });
-        }
-      );
+      jwt.sign(payload, "randomString", { expiresIn: 10000 }, (err, token) => {
+        if (err) throw err;
+        res.status(200).json({ token });
+      });
     } catch (err) {
       console.log(err.message);
       res.status(500).send("Error in Saving");
@@ -79,25 +49,53 @@ router.post(
   }
 );
 
-module.exports = router;
+router.post(
+  "/login",
+  [
+    check("email", "Please enter a valid email address.").isEmail(),
+    check("password", "Please enter a valid password").isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (!user) return res.status(400).json({ message: "User does not exist." });
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) return res.status(400).json({ message: "Incorrect password" });
+      const payload = { user: { id: user.id } };
 
-router.route("/allusers").get((req, res) => {
-  //get all users
-  User.find()
-    .then((allUsers) => res.json(allUsers))
-    .catch((err) => res.status(400).json(`Error! ${err}`));
-});
+      jwt.sign(payload, "randomString", { expiresIn: 3600 }, (err, token) => {
+        if (err) throw err;
+        res.status(200).json({ token });
+      });
+    } catch (e) {
+      console.error(e);
+      res.status(500).json({ message: "Server Error" });
+    }
+  }
+);
 
-router.route("/delete/:id").delete((req, res) => {
-  User.deleteOne({ _id: req.params.id })
-    .then((success) => res.json("Success! User removed."))
-    .catch((err) => res.status(400).json(`Error! ${err}`));
-});
+// router.route("/allusers").get((req, res) => {
+//   //get all users
+//   User.find()
+//     .then((allUsers) => res.json(allUsers))
+//     .catch((err) => res.status(400).json(`Error! ${err}`));
+// });
 
-router.route("/update/:id").put((req, res) => {
-  User.findByIdAndUpdate(req.params.id, req.body)
-    .then((user) => res.json(`Success! User updated.`))
-    .catch((err) => res.status(400).json(`Error! ${err}`));
-});
+// router.route("/delete/:id").delete((req, res) => {
+//   User.deleteOne({ _id: req.params.id })
+//     .then((success) => res.json("Success! User removed."))
+//     .catch((err) => res.status(400).json(`Error! ${err}`));
+// });
+
+// router.route("/update/:id").put((req, res) => {
+//   User.findByIdAndUpdate(req.params.id, req.body)
+//     .then((user) => res.json(`Success! User updated.`))
+//     .catch((err) => res.status(400).json(`Error! ${err}`));
+// });
 
 module.exports = router;
--- models/user.model.js
+++ models/user.model.js
@@ -5,7 +5,7 @@ const userSchema = new Schema({
   username: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
-  createdAt: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now() },
   age: { type: Number },
 });
 
